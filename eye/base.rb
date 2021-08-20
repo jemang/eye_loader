85932685685b0530afce20ed5e5bf53bb0d31369
localhost <localhost@FCNAPP10.fcn.my>
2021-08-20 17:07:31 +0800

    update eye conf

--- eye/base.rb
+++ eye/base.rb
@@ -1,4 +1,4 @@
-working_dir "#{env['FILE_PATH'}"
+working_dir "#{env['FILE_PATH']}"
 # working_dir File.expand_path(File.join(File.dirname(__FILE__), %w[ ../my_balance_mina/current/ ]))
 stdall 'log/trash.log' # stdout,err logs for processes by default
 env 'RAILS_ENV' => 'production' # global env for each processes
--- eye/puma.rb
+++ eye/puma.rb
@@ -1,8 +1,9 @@
 process :puma do
+  mode = env['MODE'].present? ? env['MODE'] : 'production'
   daemonize true
   pid_file 'tmp/pids/server.pid'
   start_grace 5.seconds
-  start_command "bundle exec puma -p #{env['PORT']} -e production"
+  start_command "bundle exec puma -p #{env['PORT']} -e #{mode}"
   stop_signals [:QUIT, 10.seconds, :TERM, 5.seconds, :KILL]
 
   # check :http, url: 'http://127.0.0.1:8000',
